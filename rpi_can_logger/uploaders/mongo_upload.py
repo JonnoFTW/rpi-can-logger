1fee8d2a2f00d5f13587e9a99fadf4f317af43f9
Jonathan Mackenzie <jonmac1@gmail.com>
2017-07-20 13:42:36 +0930

    updated mongo

--- rpi_can_logger/uploaders/mongo_upload.py
+++ rpi_can_logger/uploaders/mongo_upload.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+from __future__ import print_function
 import pymongo
 import yaml
 from glob import glob
 import csv
 import os
+import netifaces as ni
 
 with open('./mongo_conf.yaml', 'r') as conf_fd:
     conf = yaml.load(conf_fd)
@@ -15,7 +17,17 @@ log_dir = conf['log_dir']
 client = pymongo.MongoClient(mongo_uri, w=0)
 
 rpi_readings_collection = client[mongo_database][mongo_collection]
+rpi_info = client[mongo_database]['rpi-info']
+from datetime import datetime
+def get_serial():
+    with open('/proc/cpuinfo', 'r') as cpu_info:
+        return cpu_info.readlines()[-1].strip().split(' ')[-1]
 
+serial =  get_serial()
+info = {'ip':ni.ifaddresses('wlan0')[ni.AF_INET][0]['addr'],'ts':datetime.now(), 'serial': serial}
+import yaml
+print(yaml.dump(info, default_flow_style=False))
+rpi_info.insert_one(info)
 # start putting everything we've seen in the db
 for fname in sorted(glob(log_dir+'/*.csv'))[:-1]:
     # make sure this file isn't open by another process
@@ -23,11 +35,14 @@ for fname in sorted(glob(log_dir+'/*.csv'))[:-1]:
         reader = csv.DictReader(data_fh)
         trip_id = os.path.split(fname)[-1].split('.')[0]
         rpi_readings_collection.remove({'trip_id': trip_id})
-        print("Importing", trip_id)
+        # read up all the docs
+        print("Importing", trip_id, end='')
+        rows = []
         for row in reader:
             to_insert = {'trip_id': trip_id}
             to_insert.update(row)
-            rpi_readings_collection.insert_one(to_insert)
+            rows.append(to_insert)
+        rpi_readings_collection.insert_many(rows, ordered=False)
     # delete the file
     os.remove(fname)
 
