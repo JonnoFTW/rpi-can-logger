8b3f2594e48269361f7859079143c048e53d6b10
Jonathan Mackenzie <jonmac1@gmail.com>
2017-08-03 15:12:23 +0930

    edited uploader

--- rpi_can_logger/uploaders/mongo_upload.py
+++ rpi_can_logger/uploaders/mongo_upload.py
@@ -16,7 +16,7 @@ mongo_uri = conf['mongo_uri']
 mongo_database = conf['mongo_database']
 mongo_collection = conf['mongo_collection']
 log_dir = conf['log_dir']
-client = pymongo.MongoClient(mongo_uri, w=0, ssl=True)
+client = pymongo.MongoClient(mongo_uri, w=0)
 
 rpi_readings_collection = client[mongo_database][mongo_collection]
 rpi_info = client[mongo_database]['rpi-info']
@@ -30,6 +30,7 @@ rpi_info.insert_one(info)
 # start putting everything we've seen in the db
 for fname in sorted(glob(log_dir + '/*.csv'))[:-1]:
     # make sure this file isn't open by another process
+    first_row = True
     with open(fname, 'r') as data_fh:
         reader = csv.DictReader(data_fh)
         trip_id = os.path.split(fname)[-1].split('.')[0]
@@ -37,8 +38,13 @@ for fname in sorted(glob(log_dir + '/*.csv'))[:-1]:
         # read up all the docs
         print("Importing", trip_id, end='')
         rows = []
+        vid = serial
         for row in reader:
-            to_insert = {'trip_id': trip_id}
+            if first_row:
+                if 'vid' in row:
+                    vid = row['vid']
+                first_row = False
+            to_insert = {'trip_id': trip_id, 'vid': vid}
             to_insert.update(row)
             rows.append(to_insert)
         rpi_readings_collection.insert_many(rows, ordered=False)
