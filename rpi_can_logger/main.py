594071c462e17337fd42d807291826e22474b25d
Jonathan Mackenzie <jonmac1@gmail.com>
2017-09-12 12:01:17 +0930

    added log

--- rpi_can_logger/main.py
+++ rpi_can_logger/main.py
@@ -78,10 +78,13 @@ log_level = args.log_level
 is_tesla = args.tesla
 if is_tesla:
     from rpi_can_logger.logger import tesla_pids as pids, tesla_name2pid as name2pid
+    logging.warning("USING TESLA")
 elif args.fms:
     args.sniffing = True
+    logging.warning("USING FMS")
     from rpi_can_logger.logger import fms_pids as pids, fms_name2pid as name2pid
 else:
+    print("USING OBD")
     from rpi_can_logger.logger import obd_pids as pids, obd_name2pid as name2pid
     args.sniffing = True
 
