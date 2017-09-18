a01b06bf4bafa5108b534434bcb90cb9a57448b8
Jonathan Mackenzie <jonmac1@gmail.com>
2017-09-18 12:31:40 +0930

    updated playback

--- tests/playback_trc.py
+++ tests/playback_trc.py
@@ -5,24 +5,26 @@ import can
 import atexit
 import re
 from util import get_args
-interface, channel = get_args()
 
+interface, channel = get_args()
 bus = can.interface.Bus(channel=channel, interface=interface)
 atexit.register(bus.shutdown)
-if len(sys.argv) != 2:
-    exit("Please provide a pcan trace file to play back")
+print(sys.argv)
+if len(sys.argv) < 2:
+    exit("Please provide a PCAN trace file to play back")
+fname = sys.argv[2]
 #
-fname = '/scratch/Dropbox/obd/lonsdale_Test/scania_2450_loop.trc'
-fname = '/scratch/Dropbox/obd/tesla.trc'
+# fname = '/scratch/Dropbox/obd/lonsdale_Test/scania_2450_loop.trc'
+# fname = '/scratch/Dropbox/obd/tesla.trc'
 last_sleep = 0
-with open(sys.argv[1], 'r') as infile:
+with open(fname, 'r') as infile:
     print("Playing back", infile.name)
     [infile.readline() for _ in range(16)]
     for row in infile:
         row = re.split(r'\s+', row.strip())
         ts = float(row[1])
-        sleep_ms = ts-last_sleep
-        actual_sleep = max(0, sleep_ms/1000.)
+        sleep_ms = ts - last_sleep
+        actual_sleep = max(0, sleep_ms / 1000.)
         print("sleeping for ", round(sleep_ms, 3), "ms")
         time.sleep(actual_sleep)
         last_sleep = ts
