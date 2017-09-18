49e398870a74e403ba8dfa118ef3555e63d50aff
Jonathan Mackenzie <jonmac1@gmail.com>
2017-09-18 12:56:38 +0930

    updated phev query

--- tests/phev_query.py
+++ tests/phev_query.py
@@ -10,13 +10,6 @@ bus = can.interface.Bus(channel=channel, bustype=interface)
 atexit.register(bus.shutdown)
 print("Querying PHEV battery on ", channel, interface)
 
-pids = {
-    'Battery Health': [0x761, 0x762],
-    'Charges': [0x765, 766],
-    'Front RPM': [0x753, 0x754],
-    'Rear RPM': [0x755, 0x756]
-
-}
 for response_addr, pid_d in outlander_pids.items():
     msg = can.Message(arbitration_id=pid_d['request'],
                       data=[2, 0x21, 0x01, 0, 0, 0, 0, 0])
@@ -24,8 +17,19 @@ for response_addr, pid_d in outlander_pids.items():
     print("Requesting:\t", pid_d['request'])
     print(msg)
     bus.send(msg)
+    buf = bytes()
     for i in range(100):
         recvd = bus.recv()
         if recvd.arbitration_id == response_addr:
             print(recvd)
-            print(pid_d['parse'](recvd.data))
+            if response_addr == 0x762:
+                sequence = recvd.data[0]
+                if sequence == 0x10:
+                    buf += recvd.data[4:]
+                else:
+                    buf += recvd.data[1:]
+                if sequence == 0x27:
+                    print(pid_d['parse'](recvd.data))
+            else:
+                print(pid_d['parse'](recvd.data))
+
