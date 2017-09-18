e18712899815a15d7c1748cd7119b3d45a2a7092
Jonathan Mackenzie <jonmac1@gmail.com>
2017-09-18 12:32:53 +0930

    updated playback

--- tests/playback_trc.py
+++ tests/playback_trc.py
@@ -7,7 +7,7 @@ import re
 from util import get_args
 
 interface, channel = get_args()
-bus = can.interface.Bus(channel=channel, interface=interface)
+bus = can.interface.Bus(channel=channel, bustype=interface)
 atexit.register(bus.shutdown)
 print(sys.argv)
 if len(sys.argv) < 2:
