31bebad4c84553262a165123dfa70b7c7c55d577
Jonathan Mackenzie <jonmac1@gmail.com>
2017-09-19 10:47:29 +0930

    fixed stuff

--- tests/phev_query.py
+++ tests/phev_query.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from .util import get_args
+from util import get_args
 import can
 import atexit
 from rpi_can_logger.logger import outlander_pids
@@ -12,7 +12,7 @@ print("Querying PHEV on ", channel, interface)
 
 for response_addr, pid_d in outlander_pids.items():
     pid = pid_d['pid']
-    msg = can.Message(arbitration_id=pid_d['request'],
+    msg = can.Message(arbitration_id=pid_d['request'], extended_id=0,
                       data=[2, 0x21, pid, 0, 0, 0, 0, 0])
     # listen for responses on addrs[1]
     print("Requesting:\t", pid_d['request'], pid_d['name'])
@@ -27,10 +27,13 @@ for response_addr, pid_d in outlander_pids.items():
                 sequence = recvd.data[0]
                 if sequence == 0x10:
                     buf += recvd.data[4:]
+                    bus.send(can.Message(arbitration_id=0x761, extended_id=0,
+                                         data=[0x30, 0x08, 0x0a, 0, 0, 0, 0, 0]))
                 else:
                     buf += recvd.data[1:]
                 if sequence == 0x27:
-                    print(pid_d['parse'](recvd.data))
+                    print(pid_d['parse'](buf))
+                    buf = bytes()
             else:
                 print(pid_d['parse'](recvd.data))
 
