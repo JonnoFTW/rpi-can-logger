75c3e233fe45ed006f33bf26eccb7218e9fa49b3
Jonathan Mackenzie <jonmac1@gmail.com>
2017-09-19 11:27:43 +0930

    did phev test

--- tests/phev_query.py
+++ tests/phev_query.py
@@ -3,37 +3,53 @@ from util import get_args
 import can
 import atexit
 from rpi_can_logger.logger import outlander_pids
-
+from itertools import cycle
 interface, channel = get_args()
-
 bus = can.interface.Bus(channel=channel, bustype=interface)
 atexit.register(bus.shutdown)
+import time
+fst = 1
 print("Querying PHEV on ", channel, interface)
-
-for response_addr, pid_d in outlander_pids.items():
+for response_addr, pid_d in cycle([list(outlander_pids.items())[0]]):
     pid = pid_d['pid']
     msg = can.Message(arbitration_id=pid_d['request'], extended_id=0,
                       data=[2, 0x21, pid, 0, 0, 0, 0, 0])
     # listen for responses on addrs[1]
-    print("Requesting:\t", pid_d['request'], pid_d['name'])
-    print(msg)
+#    print("Requesting:\t", pid_d['request'], pid_d['name'])
+#    print(msg)
     bus.send(msg)
     buf = bytes()
-    for i in range(100):
+    num_bytes = 0
+    multiline = True
+    for i in range(500):
         recvd = bus.recv()
         if recvd.arbitration_id == response_addr:
-            print(recvd)
-            if response_addr == 0x762:
-                sequence = recvd.data[0]
-                if sequence == 0x10:
-                    buf += recvd.data[4:]
-                    bus.send(can.Message(arbitration_id=0x761, extended_id=0,
-                                         data=[0x30, 0x08, 0x0a, 0, 0, 0, 0, 0]))
-                else:
-                    buf += recvd.data[1:]
-                if sequence == 0x27:
-                    print(pid_d['parse'](buf))
+#            print("R>", recvd)
+            sequence = recvd.data[0]
+            if sequence == 0x10:
+                buf += recvd.data[4:]
+                multiline = True
+                num_bytes = recvd.data[1] - 2
+ #               print("Multiline bytes expected", num_bytes)
+
+                ctl_msg = can.Message(arbitration_id=pid_d['request'], extended_id=0,
+                                      data=[0x30, 0x08, 0x0a, 0, 0, 0, 0, 0])
+                bus.send(ctl_msg)
+#                print("S>", ctl_msg)
+            elif multiline:
+                buf += recvd.data[1:]
+#                print(len(buf), buf)
+                if len(buf) >= num_bytes:
+                    if fst:
+                        print(','.join(pid_d['parse'](buf).keys()))
+                        fst = 0
+                    print(','.join(map(str,pid_d['parse'](buf).values())))
+        
                     buf = bytes()
+                    num_bytes = 0
+                    multiline = False
+                    break
             else:
                 print(pid_d['parse'](recvd.data))
+
 
