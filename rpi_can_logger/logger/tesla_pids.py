cf560108d9dbe2bbf10679313080d7c288eda550
Jonathan Mackenzie <jonmac1@gmail.com>
2017-10-09 11:36:47 +1030

    fixed tesla field strings, converted odometer to km and added credit

--- rpi_can_logger/logger/tesla_pids.py
+++ rpi_can_logger/logger/tesla_pids.py
@@ -1,7 +1,13 @@
-import inspect
-import re
 from itertools import cycle
 
+"""
+Decoders adapted from:
+ Tesla Model S CAN Bus Deciphering
+ Jason Hughes, 2016:
+https://skie.net/uploads/TeslaCAN/Tesla%20Model%20S%20CAN%20Deciphering%20-%20v0.1%20-%20by%20wk057.pdf
+
+"""
+KM_PER_MILE = 1.60934
 
 def PID_TESLA_BMS_CUR_VOLTAGE(msg):
     return [
@@ -23,7 +29,7 @@ def PID_TESLA_REAR_DRIVE_UNIT_TORQUE_STATUS(msg):
         (msg[1] & 0x70) >> 4,
         (msg[3] & 0x70) >> 4,
         ((msg[0] + ((msg[1] & 0xF) << 8)) - (512 * (msg[1] & 0x8))) / 2,
-        (((msg[2] + ((msg[3] & 0xf) << 8)) - 500) / 20) * 1.60934  # convert to a superior unit
+        (((msg[2] + ((msg[3] & 0xf) << 8)) - 500) / 20) * KM_PER_MILE # convert to a superior unit
     ]
 
 
@@ -58,7 +64,7 @@ def PID_TESLA_BATTERY_POWER_LIMITS(msg):
 
 def PID_TESLA_BATTERY_ODOMETER(msg):
     return [
-        (msg[0] + (msg[1] << 8) + (msg[2] << 16) + (msg[3] << 24)) / 1000.0
+        ((msg[0] + (msg[1] << 8) + (msg[2] << 16) + (msg[3] << 24)) / 1000.0) * KM_PER_MILE
     ]
 
 
@@ -136,45 +142,45 @@ class PID:
 
 
 _pids = [
-    PID(0x0102, 'PID_TESLA_BMS_CUR_VOLTAGE', PID_TESLA_BMS_CUR_VOLTAGE, ['battery_voltage',
-                                                                         'regen_current',
-                                                                         'temp']),
+    PID(0x0102, 'PID_TESLA_BMS_CUR_VOLTAGE', PID_TESLA_BMS_CUR_VOLTAGE, ['batteryVoltage V',
+                                                                         'regenCurrent A',
+                                                                         'temp C']),
     PID(0x0106, 'PID_TESLA_REAR_DRIVE_UNIT_INFO', PID_TESLA_REAR_DRIVE_UNIT_INFO, ['motorRPM',
-                                                                                   'pedalPos']),
-    PID(0x0154, 'PID_TESLA_REAR_DRIVE_UNIT_MEASUREMENT', PID_TESLA_REAR_DRIVE_UNIT_MEASUREMENT, ['torqueMeasured'
-                                                                                                 'pedalPosA'
-                                                                                                 'pedalPosB']),
+                                                                                   'pedalPos %']),
+    PID(0x0154, 'PID_TESLA_REAR_DRIVE_UNIT_MEASUREMENT', PID_TESLA_REAR_DRIVE_UNIT_MEASUREMENT, ['rearTorqueMeasured Nm',
+                                                                                                 'pedalPosA %',
+                                                                                                 'pedalPosB %']),
     PID(0x0266, 'PID_TESLA_REAR_DRIVE_UNIT_POWER', PID_TESLA_REAR_DRIVE_UNIT_POWER,
-        ['inverter12V', 'dissipation', 'drivePowerMax', 'mechPower', 'statorCurrent', 'regenPowerMax']),
+        ['inverter12V V', 'dissipation W', 'drivePowerMax kW', 'mechPower kW', 'statorCurrent A', 'regenPowerMax kW']),
     PID(0x0116, 'PID_TESLA_REAR_DRIVE_UNIT_TORQUE_STATUS', PID_TESLA_REAR_DRIVE_UNIT_TORQUE_STATUS, ['gear',
                                                                                                      'gearRequest',
-                                                                                                     'torqueEstimate',
-                                                                                                     'vehicleSpeed']),
+                                                                                                     'torqueEstimate Nm',
+                                                                                                     'vehicleSpeed km/h']),
     PID(0x01D4, 'PID_TESLA_FRONT_DRIVE_UNIT', PID_TESLA_FRONT_DRIVE_UNIT, ['torqueMeasured']),
-    PID(0x02E5, 'PID_TESLA_FRONT_DRIVE_UNIT_POWER', PID_TESLA_FRONT_DRIVE_UNIT_POWER, ['inverter12V',
-                                                                                       'dissipation',
-                                                                                       'drivePowerMax',
-                                                                                       'mechPower',
-                                                                                       'statorCurrent']),
-    PID(0x0145, 'PID_TESLA_FRONT_DRIVE_UNIT_TORQUE', PID_TESLA_FRONT_DRIVE_UNIT_TORQUE, ['torqueEstimate']),
-    PID(0x0232, 'PID_TESLA_BATTERY_POWER_LIMITS', PID_TESLA_BATTERY_POWER_LIMITS, ['maxDischargePower'
-                                                                                   'maxRegenPower']),
-    PID(0x0562, 'PID_TESLA_BATTERY_ODOMETER', PID_TESLA_BATTERY_ODOMETER, ['batteryOdometer']),
-    PID(0x03D2, 'PID_TESLA_BATTERY_LIFETIME_ENERGY_STATS', PID_TESLA_BATTERY_LIFETIME_ENERGY_STATS, ['kwhChargeTotal',
-                                                                                                     'kwhDischargeTotal']),
-    PID(0x0302, 'PID_TESLA_BATTERY_STATE_OF_CHARGE', PID_TESLA_BATTERY_STATE_OF_CHARGE, ['socMin',
-                                                                                         'socUI']),
-    PID(0x0382, 'PID_TESLA_BATTERY_ENERGY_STATUS', PID_TESLA_BATTERY_ENERGY_STATUS, ['nominalFullPackEnergy',
-                                                                                     'nominalEnergyRemaining',
-                                                                                     'expectedEnergyRemaining',
-                                                                                     'idealEnergyRemaining',
-                                                                                     'energyBuffer',
-                                                                                     'energyToChargeComplete']),
-    PID(0x0210, 'PID_TESLA_DC_DC_CONVERTER_STATUS', PID_TESLA_DC_DC_CONVERTER_STATUS, ['inletTemperature',
-                                                                                       'inputPower',
-                                                                                       'outputCurrent',
-                                                                                       'outputPower',
-                                                                                       'outputVoltage']),
+    PID(0x02E5, 'PID_TESLA_FRONT_DRIVE_UNIT_POWER', PID_TESLA_FRONT_DRIVE_UNIT_POWER, ['inverter12V V',
+                                                                                       'dissipation W',
+                                                                                       'drivePowerMax kW',
+                                                                                       'mechPower kW',
+                                                                                       'statorCurrent A']),
+    PID(0x0145, 'PID_TESLA_FRONT_DRIVE_UNIT_TORQUE', PID_TESLA_FRONT_DRIVE_UNIT_TORQUE, ['torqueEstimate Nm']),
+    PID(0x0232, 'PID_TESLA_BATTERY_POWER_LIMITS', PID_TESLA_BATTERY_POWER_LIMITS, ['maxDischargePower kW',
+                                                                                   'maxRegenPower kW']),
+    PID(0x0562, 'PID_TESLA_BATTERY_ODOMETER', PID_TESLA_BATTERY_ODOMETER, ['batteryOdometer km']),
+    PID(0x03D2, 'PID_TESLA_BATTERY_LIFETIME_ENERGY_STATS', PID_TESLA_BATTERY_LIFETIME_ENERGY_STATS, ['kwhChargeTotal kWh',
+                                                                                                     'kwhDischargeTotal kWh']),
+    PID(0x0302, 'PID_TESLA_BATTERY_STATE_OF_CHARGE', PID_TESLA_BATTERY_STATE_OF_CHARGE, ['socMin %',
+                                                                                         'socUI %']),
+    PID(0x0382, 'PID_TESLA_BATTERY_ENERGY_STATUS', PID_TESLA_BATTERY_ENERGY_STATUS, ['nominalFullPackEnergy kWh',
+                                                                                     'nominalEnergyRemaining kWh',
+                                                                                     'expectedEnergyRemaining kWh',
+                                                                                     'idealEnergyRemaining kWh',
+                                                                                     'energyBuffer kWh',
+                                                                                     'energyToChargeComplete kWh']),
+    PID(0x0210, 'PID_TESLA_DC_DC_CONVERTER_STATUS', PID_TESLA_DC_DC_CONVERTER_STATUS, ['inletTemperature C',
+                                                                                       'inputPower W',
+                                                                                       'outputCurrent A',
+                                                                                       'outputPower W',
+                                                                                       'outputVoltage V']),
     PID(0x0256, 'PID_TESLA_CRUISE_CONTROL', PID_TESLA_CRUISE_CONTROL, [])
 ]
 
