e77650edde430147f00f7279d35b60e83b56b071
Jonathan Mackenzie <jonmac1@gmail.com>
2017-10-13 12:27:25 +1030

    updated stuff, swapped to gzip compressing file and api upload, added shutdown detection on logical  pin 35 for FMS shutdown on ignition loss

--- rpi_can_logger/logger/jsonlogrotator.py
+++ rpi_can_logger/logger/jsonlogrotator.py
@@ -1,11 +1,13 @@
 from datetime import datetime
-from pathlib import Path
 from io import StringIO
-import subprocess
+from glob import glob
 import logging
 import random
 import string
 import json
+import gzip
+import shutil
+import os
 from .BaseLogRotator import BaseLogRotator
 
 
@@ -25,9 +27,16 @@ class JSONLogRotator(BaseLogRotator):
         now = datetime.now()
         self.fname = self.log_folder + '/' + now.strftime('%Y%m%d_%H%M%S_{}.json'.format(self.make_random(6)))
         self._out_fh = open(self.fname, 'w')
+        self.write_pid()
+        logging.warning("Writing to {} ({} bytes)".format(self._out_fh.name, self.max_bytes))
+
+        # compress any old files still lying around
+        for fname in glob(self.log_folder+"/*.json"):
+            self._compress(fname)
+
+    def write_pid(self):
         with open(self.pid_file, 'w') as pid_out:
             pid_out.write(self.fname)
-        logging.warning("Writing to {} ({} bytes)".format(self._out_fh.name, self.max_bytes))
 
     @staticmethod
     def make_random(nchars):
@@ -37,20 +46,29 @@ class JSONLogRotator(BaseLogRotator):
     def close(self):
         self._out_fh.close()
 
+    def _compress(self, fname):
+        try:
+            with open(fname, 'rb') as f_in:
+                logging.warning("Compressing {0} into {0}.gz".format(fname))
+                with gzip.open(fname + '.gz', 'wb') as f_out:
+                    shutil.copyfileobj(f_in, f_out)
+            logging.warning("Removing {}".format(fname))
+            os.remove(fname)
+        except Exception as e:
+            logging.error(e)
+
     def writerow(self, row):
         """
 
         :param row:
         :return:
         """
-        out_txt = json.dumps(row)+"\n"
+        out_txt = json.dumps(row) + "\n"
         self._bytes_written += self._out_fh.write(out_txt)
         self._out_fh.flush()
         if self._bytes_written > self.max_bytes:
             self.close()
-            out_name = str(Path(self._out_fh.name).absolute())
-            subprocess.Popen(['7z', 'a', '-t7z', '-m0=lzma', '-mx=9', '-mfb=64', '-md=16m',
-                              out_name + '.7z', out_name])
+            self._compress(self.fname)
             self._make_writer()
 
         return out_txt
